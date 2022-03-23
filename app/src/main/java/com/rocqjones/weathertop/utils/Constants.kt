3cf875bf20ba6bdf8a01b75f416f9ffdc93d8cc2
RocqJones <jonesmbindyo@gmail.com>
2022-03-23 15:02:53 +0300

    network utilities

---
+++ app/src/main/java/com/rocqjones/weathertop/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.rocqjones.weathertop.utils
+
+object Constants {
+    /**
+     * This holds API base URL and the API key
+     */
+    const val API_KEY = ""
+    const val BASE_URL = ""
+}
