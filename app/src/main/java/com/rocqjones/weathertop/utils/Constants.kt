5597611009dd1fbab8d0a6cdef16f42b9cad6520
RocqJones <jonesmbindyo@gmail.com>
2022-03-26 20:51:38 +0300

    base url & api Key

--- app/src/main/java/com/rocqjones/weathertop/utils/Constants.kt
+++ app/src/main/java/com/rocqjones/weathertop/utils/Constants.kt
@@ -4,6 +4,6 @@ object Constants {
     /**
      * This holds API base URL and the API key
      */
-    const val API_KEY = ""
-    const val BASE_URL = ""
+    const val API_KEY = "54eed0cc270401f0c1d374a8f1f6301f"
+    const val BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q="
 }
