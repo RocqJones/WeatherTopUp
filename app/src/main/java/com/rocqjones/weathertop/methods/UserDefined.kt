818d2431f551747241ffec9cb41eaee826873fc8
RocqJones <jonesmbindyo@gmail.com>
2022-03-27 01:37:44 +0300

    custom dialog animation

--- app/src/main/java/com/rocqjones/weathertop/methods/UserDefined.kt
+++ app/src/main/java/com/rocqjones/weathertop/methods/UserDefined.kt
@@ -1,6 +1,12 @@
 package com.rocqjones.weathertop.methods
 
 import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.Context
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.rocqjones.weathertop.R
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -22,5 +28,18 @@ class UserDefined {
         df = SimpleDateFormat("dd-MM-yyyy HH:mm a")
         formattedDate = df!!.format(c.time)
         return formattedDate
+    }
+
+    fun warnDialog(message: String, context: Context) {
+        val dialog = Dialog(context)
+        dialog.setContentView(R.layout.dialog_warning)
+        dialog.setCancelable(false)
+        val warnMessage = dialog.findViewById<TextView>(R.id.warnMessage)
+        val okBtn = dialog.findViewById<Button>(R.id.okBtn)
+        warnMessage.text = message
+        okBtn.setOnClickListener { v: View? -> dialog.dismiss() }
+        dialog.window!!.attributes.windowAnimations = R.style.DialogAnimation_1
+        dialog.show()
+        dialog.setCanceledOnTouchOutside(true)
     }
 }
