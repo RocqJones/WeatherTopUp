5581f26ddde1d1a41fbe3eebfccb138e8322e593
RocqJones <jonesmbindyo@gmail.com>
2022-03-27 00:08:14 +0300

    my Singleton class that makes RequestQueue last the lifetime of our app

--- app/src/main/java/com/rocqjones/weathertop/patterns/MySingleton.kt
+++ app/src/main/java/com/rocqjones/weathertop/patterns/MySingleton.kt
@@ -1,4 +1,43 @@
 package com.rocqjones.weathertop.patterns
 
-class MySingleton {
+import android.annotation.SuppressLint
+import android.content.Context
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
+
+class MySingleton private constructor(private val c: Context) {
+
+    private var requestQueue: RequestQueue?
+
+    private fun getRequestQueue(): RequestQueue {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(c.applicationContext)
+        }
+        return requestQueue as RequestQueue
+    }
+
+    // method that accepts request
+    fun <T> addToRequestQueue(request: Request<T>?) {
+        requestQueue!!.add(request)
+    }
+
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        private var mySingleton: MySingleton? = null
+
+        // ref method that return instance of this class
+        @Synchronized
+        fun getInstance(context: Context): MySingleton? {
+            if (mySingleton == null) {
+                mySingleton = MySingleton(context)
+            }
+            return mySingleton
+        }
+    }
+
+    // object of class
+    init {
+        requestQueue = getRequestQueue()
+    }
 }
