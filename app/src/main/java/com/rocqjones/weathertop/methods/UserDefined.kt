ec8dac7bf493791ba05a498280288b73bddb1027
RocqJones <jonesmbindyo@gmail.com>
2022-03-27 01:48:33 +0300

    disable touch outside

--- app/src/main/java/com/rocqjones/weathertop/methods/UserDefined.kt
+++ app/src/main/java/com/rocqjones/weathertop/methods/UserDefined.kt
@@ -3,7 +3,6 @@ package com.rocqjones.weathertop.methods
 import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.Context
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.rocqjones.weathertop.R
