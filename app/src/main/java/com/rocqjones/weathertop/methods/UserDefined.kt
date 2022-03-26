716b56a10d9d272ee35c189d728aa334a21ee78b
RocqJones <jonesmbindyo@gmail.com>
2022-03-26 23:29:49 +0300

    date time

--- app/src/main/java/com/rocqjones/weathertop/methods/UserDefined.kt
+++ app/src/main/java/com/rocqjones/weathertop/methods/UserDefined.kt
@@ -1,11 +1,26 @@
 package com.rocqjones.weathertop.methods
 
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
+import java.util.*
+
 class UserDefined {
+
+    var c : Calendar = Calendar.getInstance()
+    private var df : SimpleDateFormat? = null
+    private var formattedDate = ""
 
     fun convertKelvinToCelsius(k: Double): Double {
         val c = 273.15
         val res = k - c
         // ans to 1 decimal place
         return String.format("%.1f", res).toDouble()
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    fun currentDateTime() : String {
+        df = SimpleDateFormat("dd-MM-yyyy HH:mm a")
+        formattedDate = df!!.format(c.time)
+        return formattedDate
     }
 }
