d6f108fd8795ed94b566668346fd77b5ff8fd895
RocqJones <jonesmbindyo@gmail.com>
2022-03-28 00:14:59 +0300

    base url trim

--- app/src/main/java/com/rocqjones/weathertop/utils/Constants.kt
+++ app/src/main/java/com/rocqjones/weathertop/utils/Constants.kt
@@ -5,5 +5,5 @@ object Constants {
      * This holds API base URL and the API key
      */
     const val API_KEY = "54eed0cc270401f0c1d374a8f1f6301f"
-    const val BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q="
+    const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 }
