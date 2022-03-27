9f0994a27fcad74bb41696e63a963f5f0519770e
RocqJones <jonesmbindyo@gmail.com>
2022-03-28 00:16:35 +0300

    model class

---
+++ app/src/main/java/com/rocqjones/weathertop/model/CitiesModel.kt
@@ -0,0 +1,9 @@
+package com.rocqjones.weathertop.model
+
+class CitiesModel {
+    var country: String? = null
+    var name: String? = null
+    var temp: String? = null
+    var speed: String? = null
+    var description: String? = null
+}
